136a89d668ad0e0f658849b4ece7c5dbc9bcf82c
bmayoral <bmayoralarauz@deloitte.co.uk>
2023-04-27 13:21:38 +0100

    First Push original Template

---
+++ sample-kit-app-template/source/extensions/deloitte.sample_app.window/deloitte/sample_app/window/window.py
@@ -0,0 +1,36 @@
+__all__ = ["MyWindow"]
+import omni.ui as ui
+
+
+class MyWindow(ui.Window):
+    """My Window"""
+
+    title = "My Window"
+
+    def __init__(self, **kwargs):
+        super().__init__(MyWindow.title, **kwargs)
+
+        # here you build the content of the window
+        with self.frame:
+            with ui.VStack():
+                ui.Label("My Great Window", height=0, style={"font_size": 48, "alignment": ui.Alignment.CENTER})
+
+                ui.Spacer()
+
+                ui.Label(
+                    """You are all awesome!""",
+                    height=0,
+                    style={"font_size": 30, "alignment": ui.Alignment.CENTER},
+                )
+
+                ui.Spacer()
+
+                with ui.HStack(height=50, style={"font_size": 24}):
+                    ui.Button("Add")
+                    ui.Button("Reset")
+
+    def show(self):
+        self.visible = True
+
+    def hide(self):
+        self.visible = False
